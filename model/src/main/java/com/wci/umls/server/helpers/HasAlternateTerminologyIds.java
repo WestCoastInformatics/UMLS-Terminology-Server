f1bd2f0b913864881b5b57079fea7a339e4ddc2a
bcarlsenca <bcarlsen@westcoastinformatics.com>
2015-04-21 13:32:33 -0700

    Add missing class.

---
+++ model/src/main/java/com/wci/umls/server/helpers/HasAlternateTerminologyIds.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2015 West Coast Informatics, LLC
+ */
+package com.wci.umls.server.helpers;
+
+import java.util.Map;
+
+/**
+ * Represents a thing that has alternate ids.
+ */
+public interface HasAlternateTerminologyIds  {
+  /**
+   * Returns the alternate terminology ids.
+   * The return value is a map of "terminology" to "terminologyId" 
+   * for terminologies other than the components native terminology.
+   * For example, the UMLS AUI applied to an atom.
+   *
+   * @return the alternate terminology ids
+   */
+  public Map<String, String> getAlternateTerminologyIds();
+  
+  /**
+   * Sets the alternate terminology ids.
+   *
+   * @param alternateTerminologyIds the alternate terminology ids
+   */
+  public void setAlternateTerminologyIds(Map<String,String> alternateTerminologyIds);
+
+  /**
+   * Put alternate terminology id.
+   *
+   * @param terminology the terminology
+   * @param terminologyId the terminology id
+   */
+  public void putAlternateTerminologyId(String terminology, String terminologyId);
+  
+  /**
+   * Removes the alternate terminology id.
+   *
+   * @param terminology the terminology
+   */
+  public void removeAlternateTerminologyId(String terminology);
+}
