1a14232686738fa455ea16dcae0a4302620956d3
bcarlsenca <bcarlsen@westcoastinformatics.com>
2015-05-05 14:23:07 -0700

    More support for transitive closure.

---
+++ admin/mojo/src/main/java/com/wci/umls/server/mojo/TransitiveClosureComputerMojo.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2015 West Coast Informatics, LLC
+ */
+package com.wci.umls.server.mojo;
+
+import java.util.Properties;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoFailureException;
+
+import com.wci.umls.server.helpers.ConfigUtility;
+import com.wci.umls.server.jpa.services.SecurityServiceJpa;
+import com.wci.umls.server.rest.client.ContentClientRest;
+import com.wci.umls.server.rest.impl.ContentServiceRestImpl;
+import com.wci.umls.server.services.SecurityService;
+
+/**
+ * Goal which recomputes transitive closure for the latest version of a
+ * specified terminology.
+ * 
+ * See admin/loader/pom.xml for sample usage
+ * 
+ * @goal compute-transitive-closure
+ * 
+ * @phase package
+ */
+public class TransitiveClosureComputerMojo extends AbstractMojo {
+
+  /**
+   * Name of terminology to compute transitive closure for.
+   * @parameter
+   * @required
+   */
+  private String terminology;
+
+  /**
+   * Version of terminology to compute transitive closure for..
+   * @parameter
+   * @required
+   */
+  private String version;
+
+  /**
+   * Whether to run this mojo against an active server
+   * @parameter
+   */
+  private boolean server = false;
+
+  /**
+   * Instantiates a {@link TransitiveClosureComputerMojo} from the specified
+   * parameters.
+   * 
+   */
+  public TransitiveClosureComputerMojo() {
+    // do nothing
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.apache.maven.plugin.Mojo#execute()
+   */
+  @Override
+  public void execute() throws MojoFailureException {
+    getLog().info("Starting computation of transitive closure");
+    getLog().info("  terminology = " + terminology);
+    getLog().info("  version = " + version);
+
+    try {
+
+      Properties properties = ConfigUtility.getConfigProperties();
+
+      boolean serverRunning = ConfigUtility.isServerActive();
+
+      getLog().info(
+          "Server status detected:  " + (!serverRunning ? "DOWN" : "UP"));
+
+      if (serverRunning && !server) {
+        throw new MojoFailureException(
+            "Mojo expects server to be down, but server is running");
+      }
+
+      if (!serverRunning && server) {
+        throw new MojoFailureException(
+            "Mojo expects server to be running, but server is down");
+      }
+
+      // authenticate
+      SecurityService service = new SecurityServiceJpa();
+      String authToken =
+          service.authenticate(properties.getProperty("admin.user"),
+              properties.getProperty("admin.password"));
+      service.close();
+
+      if (!serverRunning) {
+        getLog().info("Running directly");
+
+        ContentServiceRestImpl contentService = new ContentServiceRestImpl();
+        contentService
+            .computeTransitiveClosure(terminology, version, authToken);
+      } else {
+        getLog().info("Running against server");
+
+        // invoke the client
+        ContentClientRest client = new ContentClientRest(properties);
+        client.computeTransitiveClosure(terminology, version, authToken);
+      }
+
+      // Clean-up
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new MojoFailureException("Unexpected exception:", e);
+    }
+  }
+
+}
