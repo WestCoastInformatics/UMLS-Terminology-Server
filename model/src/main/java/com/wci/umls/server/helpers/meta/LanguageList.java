3b238dc5856472a5678a9e45d1ef380445afb622
bcarlsenca <bcarlsen@westcoastinformatics.com>
2015-05-26 18:04:54 -0700

    Add language classes.

---
+++ model/src/main/java/com/wci/umls/server/helpers/meta/LanguageList.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2015 West Coast Informatics, LLC
+ */
+package com.wci.umls.server.helpers.meta;
+
+import com.wci.umls.server.helpers.ResultList;
+import com.wci.umls.server.model.meta.Language;
+
+/**
+ * Represents a sortable list of {@link Language}
+ */
+public interface LanguageList extends ResultList<Language> {
+  // nothing extra, a simple wrapper for easy serialization
+}
