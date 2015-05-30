86184ed9a5a91c7880dbb73c313ff19ba88c22f7
bcarlsenca <bcarlsen@westcoastinformatics.com>
2015-05-29 17:51:11 -0700

    Add pfscParameter stuff.

---
+++ model/src/main/java/com/wci/umls/server/helpers/PfscParameter.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2015 West Coast Informatics, LLC
+ */
+package com.wci.umls.server.helpers;
+
+import java.util.List;
+
+/**
+ * Represents a {@link PfsParameter} with the addition of search criteria.
+ */
+public interface PfscParameter extends PfsParameter{
+
+  /**
+   * Returns the search criteria.
+   *
+   * @return the search criteria
+   */
+  public List<SearchCriteria> getSearchCriteria();
+  
+  /**
+   * Sets the search criteria.
+   *
+   * @param searchCriteria the search criteria
+   */
+  public void setSearchCriteria(List<SearchCriteria> searchCriteria);
+
+  /**
+   * Adds the search criteria.
+   *
+   * @param searchCriteria the search criteria
+   */
+  public void addSearchCriteria(SearchCriteria searchCriteria);
+}
