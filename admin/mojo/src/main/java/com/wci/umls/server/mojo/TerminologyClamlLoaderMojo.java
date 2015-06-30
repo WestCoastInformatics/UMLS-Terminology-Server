89adf4807260898e0f4d715ec15a3e86d259c61a
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2015-06-30 14:02:56 -0700

    Claml Loader

---
+++ admin/mojo/src/main/java/com/wci/umls/server/mojo/TerminologyClamlLoaderMojo.java
@@ -0,0 +1,122 @@
+package com.wci.umls.server.mojo;
+
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoFailureException;
+
+import com.wci.umls.server.helpers.ConfigUtility;
+import com.wci.umls.server.jpa.services.SecurityServiceJpa;
+import com.wci.umls.server.jpa.services.rest.ContentServiceRest;
+import com.wci.umls.server.rest.client.ContentClientRest;
+import com.wci.umls.server.rest.impl.ContentServiceRestImpl;
+import com.wci.umls.server.services.SecurityService;
+
+/**
+ * Converts claml data to RF2 objects.
+ * 
+ * See admin/loader/pom.xml for a sample execution.
+ * 
+ * @goal load-claml
+ * @phase package
+ */
+public class TerminologyClamlLoaderMojo extends AbstractMojo {
+
+  /** The date format. */
+  final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmdd");
+
+  /**
+   * The input file.
+   *
+   * @parameter 
+   * @required 
+   */
+  String inputFile;
+
+  /**
+   * Name of terminology to be loaded.
+   * @parameter
+   * @required
+   */
+  String terminology;
+
+  /**
+   * Terminology version.
+   *
+   * @parameter 
+   * @required 
+   */
+  String version;
+
+  /**
+   * Whether to run this mojo against an active server
+   * @parameter
+   */
+  private boolean server = false;
+
+ 
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.apache.maven.plugin.Mojo#execute()
+   */
+  @Override
+  public void execute() throws MojoFailureException {
+    getLog().info("Starting removing terminology");
+    getLog().info("  terminology = " + terminology);
+    getLog().info("  version = " + version);
+    getLog().info("  inputFile = " + inputFile);
+    try {
+
+      Properties properties = ConfigUtility.getConfigProperties();
+
+      boolean serverRunning = ConfigUtility.isServerActive();
+
+      getLog().info(
+          "Server status detected:  " + (!serverRunning ? "DOWN" : "UP"));
+
+      if (serverRunning && !server) {
+        throw new MojoFailureException(
+            "Mojo expects server to be down, but server is running");
+      }
+
+      if (!serverRunning && server) {
+        throw new MojoFailureException(
+            "Mojo expects server to be running, but server is down");
+      }
+
+      // authenticate
+      SecurityService service = new SecurityServiceJpa();
+      String authToken =
+          service.authenticate(properties.getProperty("admin.user"),
+              properties.getProperty("admin.password"));
+
+      if (!serverRunning) {
+        getLog().info("Running directly");
+
+        getLog().info("  Remove concepts");
+        ContentServiceRest contentService = new ContentServiceRestImpl();
+        contentService.loadTerminologyClaml(terminology, version, inputFile, authToken);
+
+
+      } else {
+        getLog().info("Running against server");
+
+        getLog().info("  Remove concepts");
+        ContentClientRest contentService = new ContentClientRest(properties);
+        contentService.loadTerminologyClaml(terminology, version, inputFile, authToken);
+
+      }
+      service.close();
+
+      getLog().info("done ...");
+      System.exit(0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new MojoFailureException("Unexpected exception:", e);
+    }
+  }
+
+}
