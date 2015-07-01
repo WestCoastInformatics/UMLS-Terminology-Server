1fb18d2bf1710f94abd8772265d11da2cc56884f
bcarlsenca <bcarlsen@westcoastinformatics.com>
2015-07-01 14:31:44 -0700

    Add classes.

---
+++ model/src/main/java/com/wci/umls/server/helpers/HasLabelSets.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2015 West Coast Informatics, LLC
+ */
+package com.wci.umls.server.helpers;
+
+import java.util.List;
+
+/**
+ * Represents a thing that has label sets.
+ */
+public interface HasLabelSets {
+  /**
+   * Returns the label sets.
+   *
+   * @return the label sets
+   */
+  public List<String> getLabels();
+
+  /**
+   * Sets the label sets.
+   *
+   * @param labelSets the label sets
+   */
+  public void setLabels(List<String> labelSets);
+
+  /**
+   * Add label set.
+   *
+   * @param labelSet the label set
+   */
+  public void addLabel(String labelSet);
+
+  /**
+   * Removes the label set.
+   *
+   * @param labelSet the label set
+   */
+  public void removeLabel(String labelSet);
+}
---
+++ model/src/main/java/com/wci/umls/server/model/meta/LabelSet.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2015 West Coast Informatics, LLC
+ */
+package com.wci.umls.server.model.meta;
+
+import com.wci.umls.server.model.content.Concept;
+
+/**
+ * Represents a set of labels for a specific purpose. See {@link Concept}.
+ */
+public interface LabelSet extends Abbreviation {
+
+  /**
+   * Returns the description.
+   *
+   * @return the description
+   */
+  public String getDescription();
+
+  /**
+   * Sets the description.
+   *
+   * @param description the description
+   */
+  public void setDescription(String description);
+
+  /**
+   * Indicates whether or not the label set is derived from another one.
+   *
+   * @return <code>true</code> if so, <code>false</code> otherwise
+   */
+  public boolean isDerived();
+
+  /**
+   * Sets the derived flag.
+   *
+   * @param derived the derived for flag
+   */
+  public void setDerived(boolean derived);
+}
