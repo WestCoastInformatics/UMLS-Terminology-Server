f44a5ee88d3541c4ff1397bbf1fc68347d192ec5
Patrick Granvold <granvold@gmail.com>
2016-03-11 11:13:52 -0800

    Fix for duplicate children on initial tree load

--- rest/src/main/java/com/wci/umls/server/rest/impl/WebsocketWorfklowListener.java
+++ rest/src/main/java/com/wci/umls/server/rest/impl/WebsocketWorfklowListener.java
@@ -1,5 +1,16 @@
 package com.wci.umls.server.rest.impl;
 
+/**
+ * The listener interface for receiving websocketWorfklow events.
+ * The class that is interested in processing a websocketWorfklow
+ * event implements this interface, and the object created
+ * with that class is registered with a component using the
+ * component's <code>addWebsocketWorfklowListener<code> method. When
+ * the websocketWorfklow event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * @see WebsocketWorfklowEvent
+ */
 //@ServiceEndpoint
 @SuppressWarnings("javadoc")
 public class WebsocketWorfklowListener {
