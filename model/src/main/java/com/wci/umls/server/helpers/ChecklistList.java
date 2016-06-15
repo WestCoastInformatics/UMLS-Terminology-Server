b8e91bf8c80cc06cf992c1268c178ca8868073fe
bcarlsenca <bcarlsen@westcoastinformatics.com>
2016-06-14 18:01:33 -0700

    NE-10: cleanup sample data.

---
+++ model/src/main/java/com/wci/umls/server/helpers/ChecklistList.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2015 West Coast Informatics, LLC
+ */
+package com.wci.umls.server.helpers;
+
+import com.wci.umls.server.model.workflow.Checklist;
+
+/**
+ * Represents a sortable list of {@link Checklist}
+ */
+public interface ChecklistList extends ResultList<Checklist> {
+  // nothing extra, a simple wrapper for easy serialization
+}
