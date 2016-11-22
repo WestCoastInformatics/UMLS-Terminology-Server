b7fa0c357ebe5dd32bfbc7e112ffbc8e936cc37c
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2016-11-22 14:29:52 -0800

    DATA: fix CCS sab

--- rest/src/main/webapp/app/page/process/editProcess.js
+++ rest/src/main/webapp/app/page/process/editProcess.js
@@ -37,8 +37,8 @@ tsApp
         // Update process
         $scope.submitProcess = function(process) {
           $scope.errors = [];
-          if (process.feedbackEmail && !validateEmail(email)) {
-            $scope.errors.push('Invalid email: ' + participant.email);
+          if (process.feedbackEmail && !validateEmail(process.email)) {
+            $scope.errors.push('Invalid email: ' + process.email);
             return;
           }
 
