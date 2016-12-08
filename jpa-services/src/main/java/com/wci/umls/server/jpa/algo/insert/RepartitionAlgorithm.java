0a4a2f0e1f1bb0d07570d6c0328d31877c412534
Rick Wood <rwood@westcoastinformatics.com>
2016-12-08 12:10:33 -0800

    NE-244: RepartitionAlgorithm changed to transactionPerOperation, since it doesn't handle multiple transactions on its own anymore.

--- jpa-services/src/main/java/com/wci/umls/server/jpa/algo/insert/RepartitionAlgorithm.java
+++ jpa-services/src/main/java/com/wci/umls/server/jpa/algo/insert/RepartitionAlgorithm.java
@@ -112,7 +112,6 @@ public class RepartitionAlgorithm extends AbstractSourceInsertionAlgorithm {
       
       logInfo("[Repartition] Clearing Current " + type + " Bins Completed");
       logInfo("[Repartition] Regenerating " + type + " Bins");
-      commitClearBegin();
 
       // reread after the commit
       project = workflowService.getProject(project.getId());
@@ -137,7 +136,6 @@ public class RepartitionAlgorithm extends AbstractSourceInsertionAlgorithm {
 
       // Update the progress
       updateProgress();
-      commitClearBegin();
 
       logInfo("[Repartition] Regenerating " + type + " Bins Completed");
 
