c4926e10c65e43bfc370f555894c37386f0a60f0
Patrick Granvold <granvold@gmail.com>
2017-01-18 14:13:30 -0800

    TypeKeyValueList/Jpa and unit test

---
+++ jpa-model/src/test/java/com/wci/umls/server/jpa/lists/TypeKeyValueListUnitTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2415 West Coast Informatics, LLC
+ */
+package com.wci.umls.server.jpa.lists;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.wci.umls.server.helpers.ProxyTester;
+import com.wci.umls.server.helpers.TypeKeyValue;
+import com.wci.umls.server.helpers.TypeKeyValueList;
+import com.wci.umls.server.jpa.helpers.TypeKeyValueJpa;
+import com.wci.umls.server.jpa.helpers.TypeKeyValueListJpa;
+
+/**
+ * Unit testing for {@link TypeKeyValueList}.
+ */
+public class TypeKeyValueListUnitTest extends AbstractListUnit<TypeKeyValue> {
+
+  /** The list1 test fixture . */
+  private TypeKeyValueList list1;
+
+  /** The list2 test fixture . */
+  private TypeKeyValueList list2;
+
+  /** The test fixture o1. */
+  private TypeKeyValue o1;
+
+  /** The test fixture o2. */
+  private TypeKeyValue o2;
+
+  /**
+   * Setup class.
+   */
+  @BeforeClass
+  public static void setupClass() {
+    // do nothing
+  }
+
+  /**
+   * Setup.
+   *
+   * @throws Exception the exception
+   */
+  @Before
+  public void setup() throws Exception {
+    list1 = new TypeKeyValueListJpa();
+    list2 = new TypeKeyValueListJpa();
+
+    ProxyTester tester = new ProxyTester(new TypeKeyValueJpa());
+    o1 = (TypeKeyValue) tester.createObject(1);
+    o2 = (TypeKeyValue) tester.createObject(2);
+
+  }
+
+  /**
+   * Test normal use of a list.
+   * @throws Exception the exception
+   */
+  @Test
+  public void testNormalUse() throws Exception {
+    testNormalUse(list1, list2, o1, o2);
+  }
+
+  /**
+   * Test degenerate use of a list. Show that the underlying data structure
+   * should NOT be manipulated.
+   * 
+   * @throws Exception the exception
+   */
+  @Test
+  public void testDegenerateUse() throws Exception {
+    testDegenerateUse(list1, list2, o1, o2);
+  }
+
+  /**
+   * Test edge cases of a list.
+   * 
+   * @throws Exception the exception
+   */
+  @Test
+  public void testEdgeCases() throws Exception {
+    testEdgeCases(list1, list2, o1, o2);
+  }
+
+  /**
+   * Test XML serialization of a list.
+   *
+   * 
+   * @throws Exception the exception
+   */
+  @Test
+  public void testXmlSerialization() throws Exception {
+    testXmllSerialization(list1, list2, o1, o2);
+  }
+
+  /**
+   * Teardown.
+   */
+  @After
+  public void teardown() {
+    // do nothing
+  }
+
+  /**
+   * Teardown class.
+   */
+  @AfterClass
+  public static void teardownClass() {
+    // do nothing
+  }
+
+}
