abdc35a9b75551b5ddef866171b2b474b1423b7b
Patrick Granvold <granvold@gmail.com>
2017-02-07 14:43:40 -0800

    Fixes to simpleediting removeConcept in report.js

--- rest/src/main/webapp/app/component/report/report.js
+++ rest/src/main/webapp/app/component/report/report.js
@@ -1,33 +1,48 @@
 // Report directive
-tsApp.directive('report', [ '$window', '$routeParams', function($window, $routeParams) {
-  return {
-    restrict : 'A',
-    scope : {
-      // selected features
-      selected : '=',
-      lists : '=',
-      // callbacks functions
-      callbacks : '='
-    },
-    templateUrl : 'app/component/report/report.html',
-    controller : [ '$scope', 'reportService', function($scope, reportService) {
-      // Scope vars
-      $scope.config = {
-        showHidden : true
-      };
+tsApp.directive('report', [
+  '$window',
+  '$routeParams',
+  function($window, $routeParams) {
+    return {
+      restrict : 'A',
+      scope : {
+        // selected features
+        selected : '=',
+        lists : '=',
+        // callbacks functions
+        callbacks : '='
+      },
+      templateUrl : 'app/component/report/report.html',
+      controller : [
+        '$scope',
+        'reportService',
+        'editService',
+        'utilService',
+        function($scope, reportService, editService, utilService) {
+          // Scope vars
+          $scope.config = {
+            showHidden : true
+          };
 
-      $scope.removeConcept = function() {
-        if ($scope.callbacks.hasOwnProperty('removeConcept')) {
-          $scope.callbacks.removeConcept($scope.selected.project.id, concept.id).then(function() {
-            // do nothing
-          })
-        } else {
-          editService.removeConcept($scope.selected.project.id, concept.id).then(function() {
-            $scope.selected.component = null;
-          });
-        }
-      }
+          // NOTE: Only applicable to simple edit mode, only for concepts so far
+          $scope.removeComponent = function() {
+            console.debug('remove component', $scope.callbacks);
 
-    } ]
-  };
-} ]);
+            if ($scope.selected.component.type != 'CONCEPT') {
+             utilService.setError('Only components of type Concept can be removed at this time')
+              return;
+            }
+
+            editService.removeConcept($scope.selected.project.id, $scope.selected.component.id)
+              .then(function() {
+                $scope.selected.component = null;
+
+                // call get component with null value to trigger callback behavior
+                $scope.callbacks.getComponent(null);
+              });
+
+          }
+
+        } ]
+    };
+  } ]);
