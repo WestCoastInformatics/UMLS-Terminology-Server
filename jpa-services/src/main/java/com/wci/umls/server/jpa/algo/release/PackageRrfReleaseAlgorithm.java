2d5e96e0fbb1a7f98f309616a203c5a0d9d8c610
shapirod2 <dshapiro@westcoastinformatics.com>
2017-02-22 06:55:37 -0800

    NE-216: PackageRrfReleaseAlgorithm

--- jpa-services/src/main/java/com/wci/umls/server/jpa/algo/release/PackageRrfReleaseAlgorithm.java
+++ jpa-services/src/main/java/com/wci/umls/server/jpa/algo/release/PackageRrfReleaseAlgorithm.java
@@ -3,45 +3,112 @@
  */
 package com.wci.umls.server.jpa.algo.release;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.io.IOUtils;
 
 import com.wci.umls.server.ValidationResult;
 import com.wci.umls.server.helpers.ConfigUtility;
+import com.wci.umls.server.jpa.ValidationResultJpa;
 import com.wci.umls.server.jpa.algo.AbstractAlgorithm;
 
 /**
- * Algorithm for packaging RRF relaese into a .zip file.
+ * Algorithm for packaging RRF release into a .zip file.
  */
 public class PackageRrfReleaseAlgorithm extends AbstractAlgorithm {
 
+  
+  /** The path. */
+  private File path;
+  
+  /** The filename. */
+  private String filename;
+  
+  /** The zip file. */
+  private File zipFile;
+  
   /**
    * Instantiates an empty {@link PackageRrfReleaseAlgorithm}.
    *
    * @throws Exception the exception
    */
   public PackageRrfReleaseAlgorithm() throws Exception {
-    // TODO Auto-generated constructor stub
+    super();
+    setActivityId(UUID.randomUUID().toString());
+    setWorkId("PACKAGERRF");
+
   }
 
   /* see superclass */
   @Override
   public ValidationResult checkPreconditions() throws Exception {
-    // TODO Auto-generated method stub
-    return null;
+       
+    path = new File(config.getProperty("source.data.dir") + "/"
+        + getProcess().getInputPath());
+    logInfo("  path " + path);
+    
+    filename = getProcess().getVersion() + ".zip";
+    zipFile = new File(path, filename);
+    logInfo("  zipFileName " + zipFile);
+    
+    
+    if (zipFile.exists()) {
+      throw new Exception(
+          "File already exists = " + zipFile.getAbsolutePath());
+    }
+    
+    return new ValidationResultJpa();
   }
 
   /* see superclass */
   @Override
   public void compute() throws Exception {
-    // TODO Auto-generated method stub
+       
+    final File pathMeta = new File(path, "/" + getProcess().getVersion() + "/META");
+    logInfo("  pathMeta " + pathMeta);
 
+    final File mmsysPath = new File(path, "/" + getProcess().getVersion() + "/MMSYS");
+    logInfo("  mmsysPath " + mmsysPath);   
+
+    ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFile)); 
+    zipDirectory(pathMeta, out, pathMeta.getPath().length() + 1 - "/META".length());     
+    zipDirectory(mmsysPath, out, mmsysPath.getPath().length() + 1);
+    
+    out.close(); 
+    
   }
 
+  public static void zipDirectory(File folder, ZipOutputStream zipOutputStream, int prefixLength)
+    throws IOException { 
+    for (final File file : folder.listFiles()) {
+      if (file.isFile()) {
+          final ZipEntry zipEntry = new ZipEntry(file.getPath().substring(prefixLength));
+          zipOutputStream.putNextEntry(zipEntry);
+          try (FileInputStream inputStream = new FileInputStream(file)) {
+              IOUtils.copy(inputStream, zipOutputStream);
+          }
+          zipOutputStream.closeEntry();
+      } else if (file.isDirectory()) {
+          zipDirectory(file, zipOutputStream, prefixLength);
+      }
+    }
+
+  }  
+  
   /* see superclass */
   @Override
   public void reset() throws Exception {
-    // TODO Auto-generated method stub
 
+    logInfo("Starting RESET " + getName());
+    // n/a
+    logInfo("Finished RESET " + getName()); 
   }
 
   /* see superclass */
