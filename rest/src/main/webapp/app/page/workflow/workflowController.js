8e129cf2dacdb572bb8b2933c7852858c8def0f8
Rick Wood <rwood@westcoastinformatics.com>
2017-08-07 11:18:41 -0700

    NE-361: If a bin regeneration is going to take longer than 2 minutes, show confirmation box before it starts.

--- rest/src/main/webapp/app/page/workflow/workflowController.js
+++ rest/src/main/webapp/app/page/workflow/workflowController.js
@@ -445,13 +445,20 @@ tsApp.controller('WorkflowCtrl', [
 
     // Regenerate single bin
     $scope.regenerateBin = function(bin) {
-      // send both id and name
-      workflowService.regenerateBin($scope.selected.project.id, bin.id, bin.name,
-        $scope.selected.config.type).then(
-      // Success
-      function(data) {
-        $scope.getBins($scope.selected.project.id, $scope.selected.config, bin);
-      });
+        // If last regeneration time was longer than 2 minutes, get confirmation
+    	var regenerate = true
+    	if((bin.creationTime/1000) > 120){
+    		regenerate = confirm('This bin took ' + bin.creationTime/1000 + ' seconds to process last time it was run. \n\n Are you sure you want to regenerate now?');
+    	}
+    	if (regenerate == true) {
+    		// send both id and name
+    	      workflowService.regenerateBin($scope.selected.project.id, bin.id, bin.name,
+    	    	        $scope.selected.config.type).then(
+    	    	      // Success
+    	    	      function(data) {
+    	    	        $scope.getBins($scope.selected.project.id, $scope.selected.config, bin);
+    	    	      });
+    	}
     };
 
     // Regenerate bins
