c88cf31bbbde9349b5e27d62a212043571ee8aeb
Rick Wood <rwood@westcoastinformatics.com>
2020-07-31 14:18:44 -0700

    NM-79: update UmlsIdentifierAssignmentHandler to correctly identify the maximum CUI now that CUIs are variable length.

--- jpa-services/src/main/java/com/wci/umls/server/jpa/services/handlers/UmlsIdentifierAssignmentHandler.java
+++ jpa-services/src/main/java/com/wci/umls/server/jpa/services/handlers/UmlsIdentifierAssignmentHandler.java
@@ -17,6 +17,7 @@ import org.hibernate.ScrollMode;
 import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 
+import com.wci.umls.server.helpers.Branch;
 import com.wci.umls.server.helpers.ComponentInfo;
 import com.wci.umls.server.helpers.ConfigUtility;
 import com.wci.umls.server.jpa.AbstractConfigurable;
@@ -210,9 +211,14 @@ public class UmlsIdentifierAssignmentHandler extends AbstractConfigurable
       try {
         final javax.persistence.Query query = service.getEntityManager()
             .createQuery("select max(terminologyId) from ConceptJpa "
-                + "where terminology = :terminology "
+                + "  where terminology = :terminology "
                 + "  and version = :version "
-                + "  and terminologyId like :prefix");
+                + "  and terminologyId like :prefix "
+                + "  and length(terminologyId) = "
+                + "  (SELECT MAX(LENGTH(terminologyId)) FROM  ConceptJpa"
+                + "  where terminology = :terminology "
+                + "  and version = :version "
+                + "  and terminologyId like :prefix)");
         query.setParameter("terminology", concept.getTerminology());
         query.setParameter("version", concept.getVersion());
         query.setParameter("prefix", prefixMap.get("CUI") + "%");
@@ -227,9 +233,22 @@ public class UmlsIdentifierAssignmentHandler extends AbstractConfigurable
       }
       // Set the maxConceptId
       maxConceptId = conceptId;
+
+      // Try looking up concept with CUI = CUIprefix + (maxConceptId + 1), to
+      // ensure that maxConceptId is truly the max
+      final Concept testConcept =
+          service.getConcept(prefixMap.get("CUI") + (maxConceptId + 1),
+              concept.getTerminology(), concept.getVersion(), Branch.ROOT);
+
+      if (testConcept != null) {
+        throw new Exception("max CUI lookup failure: " + prefixMap.get("CUI")
+            + maxConceptId + " is not the maximum CUI");
+      }
+
       Logger.getLogger(getClass())
           .info("Initializing max CUI = " + maxConceptId);
     }
+
     final long result = ++maxConceptId;
     return convertId(result, "CUI");
   }
