4d2e69cdcefade676326884be8949f762fae4371
shapirod2 <dshapiro@westcoastinformatics.com>
2021-11-12 10:33:36 -0800

    NM-123: Fix bequeathals to unpublishable concepts and PN only concepts

--- jpa-services/src/main/java/com/wci/umls/server/jpa/algo/insert/CreateAncestorBequeathalAlgorithm.java
+++ jpa-services/src/main/java/com/wci/umls/server/jpa/algo/insert/CreateAncestorBequeathalAlgorithm.java
@@ -167,7 +167,7 @@ public class CreateAncestorBequeathalAlgorithm extends AbstractInsertMaintReleas
               Long ncimthConceptId = srl.getObjects().get(0).getId();
               Concept ncimthParentConcept = getConcept(ncimthConceptId);
               
-              if (noXRRel(c, ncimthParentConcept) && ncimthParentConcept.isPublishable()) {
+              if (noXRRel(c, ncimthParentConcept) && conceptPublishable(ncimthParentConcept)) {
                 StringBuffer sb = new StringBuffer();
                 sb.append("").append("|");
                 sb.append("C").append("|");
@@ -191,7 +191,7 @@ public class CreateAncestorBequeathalAlgorithm extends AbstractInsertMaintReleas
                     Long ncimthConceptId2 = srl2.getObjects().get(0).getId();
                     Concept ncimthParentConcept2 = getConcept(ncimthConceptId2);
                    
-                    if (noXRRel(c, ncimthParentConcept2) && ncimthParentConcept2.isPublishable()) {
+                    if (noXRRel(c, ncimthParentConcept2) && conceptPublishable(ncimthParentConcept2)) {
                       StringBuffer sb = new StringBuffer();
                       sb.append("").append("|");
                       sb.append("C").append("|");
@@ -253,6 +253,11 @@ public class CreateAncestorBequeathalAlgorithm extends AbstractInsertMaintReleas
 
   }
 
+  private boolean conceptPublishable(Concept cpt) {
+	  return cpt.getAtoms().stream().filter(a -> a.isPublishable() && !a.getTerminology().equals("NCIMTH")
+			  && !a.getTerminology().startsWith("MTH_")).count() > 0;
+  }
+  
   private boolean noXRRel(Concept a, Concept b) {
     for (ConceptRelationship cr : a.getRelationships()) {
       if (cr.getRelationshipType().equals("XR") && (cr.getFrom().getId() == b.getId() || cr.getTo().getId() == b.getId())) {
