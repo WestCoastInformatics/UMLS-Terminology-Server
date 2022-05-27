65590b9b5599c3a6d89dda566e6f2fefe3e5c52d
Brian Carlsen <brian@ciitizen.com>
2022-05-27 15:01:52 -0700

    REvert back to MultipleHiLoPerTableGenerator

--- jpa-model/src/main/java/com/wci/umls/server/jpa/helpers/UseExistingOrGenerateIdGenerator.java
+++ jpa-model/src/main/java/com/wci/umls/server/jpa/helpers/UseExistingOrGenerateIdGenerator.java
@@ -1,13 +1,14 @@
 /*
- *    Copyright 2015 West Coast Informatics, LLC
+ *    Copyright 2022 West Coast Informatics, LLC
  */
 package com.wci.umls.server.jpa.helpers;
 
 import java.io.Serializable;
 
 import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.id.enhanced.TableGenerator;
+import org.hibernate.id.MultipleHiLoPerTableGenerator;
 
 import com.wci.umls.server.helpers.HasId;
 
@@ -15,12 +16,13 @@ import com.wci.umls.server.helpers.HasId;
  * Generator to create a unique ID for the object. If the object's ID is already
  * set, keep it.
  */
-public class UseExistingOrGenerateIdGenerator extends TableGenerator {
+@SuppressWarnings("deprecation")
+public class UseExistingOrGenerateIdGenerator extends MultipleHiLoPerTableGenerator {
 
   /* see superclass */
   @Override
   public synchronized Serializable generate(final SharedSessionContractImplementor session,
-    final Object object) throws HibernateException {
+    Object object) throws HibernateException {
     if (object == null) {
       throw new HibernateException(new NullPointerException());
     }
@@ -29,7 +31,7 @@ public class UseExistingOrGenerateIdGenerator extends TableGenerator {
     // HasId.
     final Long id = ((HasId) object).getId();
     // final Serializable id = session.getEntityPersister(null, object)
-    // .getClassMetadata().getIdentifier(object, session);
+    // .getClassMhetadata().getIdentifier(object, session);
     return id != null ? id : super.generate(session, object);
   }
 }
